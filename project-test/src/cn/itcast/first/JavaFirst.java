d54bbd63eb1ecbd72a1e753543ecebc568280d0b
ADLNXY <ADLNXY@users.noreply.github.com>
2018-05-23 17:06:23 +0800

    java测试项目的第一次提交, 忽略bin(字节码文件)

---
+++ project-test/src/cn/itcast/first/JavaFirst.java
@@ -0,0 +1,12 @@
+package cn.itcast.first;
+
+/****
+ * author: adlnxy
+ * date: 2018/5/23 16:55
+ * description:
+ **/
+public class JavaFirst {
+        public static void main(String[] args) {
+                System.out.println("hello, git");
+        }
+}
